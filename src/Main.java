f9183fb943de5474e89b29cb74dc8288a1b1e2ab
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-09-17 15:26:15 -0400

    created Main Class to run doFizzBuzz

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.doFizzBuzz(5);
+        String [] str_arg = {};
+        FizzBuzz.main(str_arg);
+    }
 }
