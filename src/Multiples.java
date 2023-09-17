e86af887f3a0c708c0a5bcd0e80bf9c3d5be728a
jeansnfitted <90649427+jeansnfitted@users.noreply.github.com>
2023-09-17 16:27:12 -0400

    created Multiples Class

---
+++ src/Multiples.java
@@ -0,0 +1,14 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+    public static void main(String[] args) {
+        for (Integer i = 0; i < 1000; i++) {
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            if (divisibleBy3 || divisibleBy5) {
+                System.out.println(i);
+            }
+        }
+    }
+}
